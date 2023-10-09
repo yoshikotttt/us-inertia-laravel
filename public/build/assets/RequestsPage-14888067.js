e2b5a41f1c891ed65fb19ca6826c21ac5315fd67
yoshikotttt <myqic.n@gmail.com>
2023-10-09 11:26:26 +0900

    自動更新追加

--- public/build/assets/RequestsPage-14888067.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-4723462a.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("div",{children:"RequestsPage"})});export{t as default};
--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,9 +1,32 @@
 import AcceptHome from "@/Pages/AcceptHome";
 import RequestHome from "@/Pages/RequestHome";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Inertia } from "@inertiajs/inertia";
 
-const HomePage = ({ user, notifications }) => {
+const HomePage = ({ user, notifications: initialNotifications }) => {
+    const [notifications, setNotifications] = useState(initialNotifications);
+
+     useEffect(() => {
+         // 10秒ごとに通知を確認する
+         const interval = setInterval(() => {
+             Inertia.get(
+                 "/home",
+                 {},
+                 {
+                     onSuccess: (page) => {
+                         setNotifications(page.props.notifications);
+                     },
+                 }
+             );
+         }, 100000); // 10000ms = 10s
+
+         // クリーンアップ関数
+         return () => {
+             clearInterval(interval);
+         };
+     }, []);
+
     return (
         <>
             <Authenticated user={user}>
